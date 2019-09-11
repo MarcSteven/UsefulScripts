1488b7cf29da2e60395839fe8e00befa764fe25f
Marc Steven <MarcSteven@users.noreply.github.com>
2019-09-11 09:56:26 +0800

    upload the file to generate execute file

---
+++ UsefulScripts/generate_execute_file.rb
@@ -0,0 +1,8 @@
+# !/usr/bin/env ruby -w
+#
+path = ARGV[0]
+fail "specify filename to create" unless  path
+
+File.open(path,"w") { |f| puts "#!/usr/bin/env ruby -w"}
+File.chmod(0755,path)
+system "open", path
